f02e03edeb88aead530a3627fe9b344349341f98
FreeBz <freebz@hananet.net>
2017-10-17 17:12:56 +0900

    Chapter 1 Introducing Clojure

---
+++ ch01/ex1-1.clj
@@ -0,0 +1,19 @@
+(get-url "http://example.com")
+
+
+(str "Hello, " "World!")
+;; Result: "Hello, World!"
+; Semi-colon starts a code comment which continues
+; to the end of the line.)
+
+
+(+ 1 2)
+;; Result: 3
+(+ 1 2 3)
+;; Result: 6
+
+
+(+ 3 (* 4 2))
+;; Result: 11
+
+
---
+++ ch01/ex1-2.clj
@@ -0,0 +1,14 @@
+(. Math PI)
+;; Result: 3.141592...
+(. Math abs -3)
+;; Result: 3
+(. "foo" toUpperCase)
+;; Result: "FOO"
+
+(.toUpperCase "foo")
+;; Result: "FOO"
+
+(new Integer "42")
+;; Result: 42
+(Integer. "42")
+;; Result: 42
